a36516eec19cf8020e10621b3808c8d7114e0326
Raj Saini <navraj.saini@uleth.ca>
2017-01-30 19:10:52 -0700

    added the test files (.h and .cpp)

---
+++ src/TestCoordinate.cpp
@@ -0,0 +1,4 @@
+#include "TestCoordinate.h"
+#include <iostream>
+CPPUNIT_TEST_SUITE_REGISTRATION(TestCoordinate);
+
---
+++ test/TestCoordinate.h
@@ -0,0 +1,14 @@
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+#include <iostream>
+#include "Coordinate.h"
+class TestCoordinate : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE();
+    CPPUNIT_TEST();
+    CPPUNIT_TEST_SUITE_END();
+
+
+};
+#endif // COORDINATE_H
