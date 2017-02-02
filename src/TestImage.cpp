6a77c45702d1d88ca3082ee14eadd47e2562c649
Raj Saini <navraj.saini@uleth.ca>
2017-02-01 20:43:07 -0700

    added a bug for Window class

--- src/TestImage.cpp
+++ src/TestImage.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION(TestImage);
-/*
-    TestImage: check to see if widget_error is thrown when checking if the c (1, 1) Coordinate is within the A (5,5) coordinate
-*/
+/**
+*    TestImage: check to see if widget_error is thrown when checking if the c (1, 1) Coordinate is within the A (5,5) coordinate
+**/
 void TestImage::testImage()
 {
     Coordinate c(1, 1);
@@ -17,9 +17,9 @@ void TestImage::testImage()
     Image i(c, 1, 1);
     CPPUNIT_ASSERT_THROW(i.getAt(a), widget_error);
 }
-/*
-    test1: check if the image made within the coordinate sends an error
-*/
+/**
+*    test1: check if getAt returns a capital I when the coordinate is within the image
+**/
 void TestImage::test1()
 {
     Coordinate c(1, 1);
@@ -27,4 +27,20 @@ void TestImage::test1()
 
     Image i(c, 1, 1);
     CPPUNIT_ASSERT(i.getAt(c)=='I');
+}
+/**
+    test2: check the edge coordinates for the image created
+*/
+void TestImage::test2()
+{
+    Coordinate c(1, 1);
+    Coordinate a(0, 1);
+    Coordinate b(1, 0);
+    Coordinate d(0, 0);
+
+    Image i(c, 1, 1);
+    CPPUNIT_ASSERT_NO_THROW(i.getAt(c));
+    CPPUNIT_ASSERT_NO_THROW(i.getAt(a));
+    CPPUNIT_ASSERT_NO_THROW(i.getAt(b));
+    CPPUNIT_ASSERT_NO_THROW(i.getAt(d));
 }
--- test/TestImage.h
+++ test/TestImage.h
@@ -9,13 +9,13 @@ class TestImage : public CppUnit::TestFixture
     CPPUNIT_TEST_SUITE(TestImage);
     CPPUNIT_TEST(testImage);
     CPPUNIT_TEST(test1);
-//    CPPUNIT_TEST(test2);
+    CPPUNIT_TEST(test2);
     CPPUNIT_TEST_SUITE_END();
 
     public:
         void testImage();
-        void test1(); // constructor test
- //       void test2(); // test getAT function
+        void test1();
+        void test2();
     private:
 
 };
